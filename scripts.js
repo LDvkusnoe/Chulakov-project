8a4461c00c474248cacb78e0d444e31027f7c207
Erhov Sergey <LDvkusnoe@yandex.ru>
2021-01-26 22:40:33 +0300

    переделал js

--- scripts.js
+++ scripts.js
@@ -1,144 +1,51 @@
-let inputs = document.getElementsByTagName('input');
-addEventListnersToInputs(inputs);
+let elms = [...document.querySelectorAll('.card-number__data')].concat(document.querySelector('.cardback__cvv'), document.querySelector('.card-name__holder')),
 
-function onKeyUp(e) {
-    let validateParams = null;
-    switch (e.target.id) {
-        case 'numberplace1':
-        {
-            validateParams = {
-                RegExpString: "[0-9]",
-                UppedKeyCode: e.keyCode,        
-            }
-            break;
-        }
-        case 'numberplace2':
-        {
-            validateParams = {
-                RegExpString: "[0-9]",
-                UppedKeyCode: e.keyCode,
-            }
-            break;
-        }
-        case 'numberplace3':
-        {
-            validateParams = {
-                RegExpString: "[0-9]",
-                UppedKeyCode: e.keyCode,
-            }
-            break;
-        }
-        case 'numberplace4':
-        {
-            validateParams = {
-                RegExpString: "[0-9]",
-                UppedKeyCode: e.keyCode,
-            }
-            break;
-        }
+require_lengths = [4, 4, 4, 4, 3, 4],
 
-        case 'cardname':
-        {
-            validateParams = {
-                RegExpString: "[a-zA-Z\\s]",
-                IsUpperCase: true
-            }
-            break;
-        }
-
-
-        case 'cvv':
-        {
-            validateParams = {
-                RegExpString: "[0-9]", 
-            }
-            break;
-        }
-    }
-
-    e.target.value = validateParams ? inputValidate(e.target.value, validateParams) : '';
-}
-
-function inputValidate(value, validateParams) {
-    var regexp = new RegExp(validateParams.RegExpString, 'g')
-    let validInput = '';
-    let backspaceKeyCode = 8;
-    let validCharsArray = value.match(regexp);
-
-    if (validCharsArray) {
-        validCharsArray.forEach(function (char, index) {
-            let currentCharNumber = index + 1;
-            if (currentCharNumber > validateParams.MaxSymbolsCount) {
-                return;
-            }
-            validInput += char;
-        });
-    }
-
-    return validateParams.IsUpperCase ? validInput.toUpperCase() : validInput;
-}
-
-function addEventListnersToInputs(inputs) {
-    let inputsArray = Array.from(inputs);
-    inputsArray.forEach(function (input) {
-        input.addEventListener('keyup', {
-            handleEvent: onKeyUp
-        })
-    });
-}
-
-function on_input (e) {
-    let next = e.target.nextElementSibling,
-        prev = e.target.previousElementSibling
-    if (e.target.value.length >= 4 && next && next.classList.contains('card-number__data'))
-      next.focus ()
-    if (e.target.value.length <= 0 && prev && prev.classList.contains('card-number__data'))
-      prev.focus ()
+error_style = {
+  add : elm => elm.classList.add('error'),
+  del : elm => elm.classList.remove('error')
+},
+val = {
+  empty : (e) => {
+    e.preventDefault ()
+    elms.forEach(elm => !elm.value ? error_style.add(elm) : ``)
+  },
+  wrong_length : (e) => {
+    e.preventDefault ()
+    elms.forEach((elm, i) => elm.value.replace(/ /g, ``).length < require_lengths[i] ? error_style.add(elm) : ``)
   }
-  document.querySelector('.card-number').addEventListener('input', on_input)
+}
 
 
-
-  function sendData() {
-    let errors = '';
-    if (document.getElementById('numberplace1').value.length < 4) {
-        
-        errors += 'Заполните поле номер карты 1\n';
-    }
-
-    if (document.getElementById('numberplace2').value.length < 4) {
-        
-        errors += 'Заполните поле номер карты 2\n';
-    }
-
-    if (document.getElementById('numberplace3').value.length < 4) {
-        
-        errors += 'Заполните поле номер карты 3\n';
-    }
-
-    if (document.getElementById('numberplace4').value.length < 4) {
-        
-        errors += 'Заполните поле номер карты 4\n';
-    }
-
-    if (document.getElementById('cvv').value.length < 3) {
-        errors += 'Заполните поле cvv \n';
-        
-    }
-
-    if (document.getElementById('cardname').value.length < 4) {
-        
-        errors += 'Введите корректное имя \n';
-        
-    }
-
-    if (errors) {
-        alert('Ошибка \n' + errors);
-        return;
-    }
+function toggle_input (e) {
+  let next = e.target.nextElementSibling,
+      prev = e.target.previousElementSibling
+  if (e.target.value.length >= 4 && next && next.classList.contains('card-number__data')) next.focus ()
+  if (e.target.value.length <= 0 && prev && prev.classList.contains('card-number__data')) prev.focus ()
+}
+function only_numbers (e, count) {
+  if (e.target.classList.contains('card-number__data') || e.target.classList.contains('cardback__cvv'))
+    e.target.value = e.target.value.replace(/\D/g,'').substr(0, count)
+}
+function only_latin_and_upper_case () {
+  if (this.classList.contains('card-name__holder')) {
+    this.value = this.value.toUpperCase ()
+    this.value = this.value.replace(/^ +|[^A-Z ]/g, '').replace(/ +(\w*).*/, ' $1')
   }
+}
+function on_submit_form (e) {
+  elms.forEach(elm => error_style.del(elm))
+  elms.some((elm, i) => !elm.value || elm.value.replace(/ /g, ``).length < require_lengths[i]) && (val.empty(e), val.wrong_length(e))
+}
+function on_blur () {
+  if (!this.value || this.value.replace(/ /g, ``).length < require_lengths[elms.indexOf(this)]) error_style.add(this)
+  else error_style.del(this)
+}
 
 
-
- 
-
+elms.forEach(elm => elm.addEventListener('blur', on_blur))
+document.querySelector('.card-number').addEventListener('input', e => (only_numbers (e, 4), toggle_input (e)) )
+document.querySelector('.cardback__cvv').addEventListener('input', e => only_numbers (e, 3) )
+document.querySelector('.card-name__holder').addEventListener('input', only_latin_and_upper_case )
+document.querySelector('.elements').addEventListener('submit', on_submit_form)
